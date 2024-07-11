6f5bfb054b9867588cca0c74b2a400102a3376f4
koreamarin <awldnjs2@gmail.com>
2024-07-11 12:18:54 +0900

    Feat : queryDSL 완강, 세션7 코드 적용안함

--- src/main/java/study/querydsl/controller/MemberController.java
+++ src/main/java/study/querydsl/controller/MemberController.java
@@ -19,16 +19,20 @@ public class MemberController {
     private final MemberJpaRepository memberJpaRepository;
     private final MemberRepository memberRepository;
 
+    // Spring Data JPA를 사용하지 않고 member에 관한 정보를 memberTeamDto로 출력한 방식
     @GetMapping("/v1/members")
     public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
         return memberJpaRepository.searchByWhere(condition);
     }
 
+    // Spring Data JPA를 사용한 방식. fetchResults()를 사용하여 MemberTeamDto를 출력한다.
     @GetMapping("/v2/members")
     public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
         return memberRepository.searchPageSimple(condition, pageable);
     }
 
+    // Spring Data JPA를 사용한 방식. fetchResults()를 사용하지 않고, content와 total을 따로 출력하였으며,
+    // PageableExecutionUtils를 사용하여 count쿼리를 생략 가능한 경우 생략해서 사용하는 페이징 처리 최적화 기법.
     @GetMapping("/v3/members")
     public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
         return memberRepository.searchPageComplex(condition, pageable);
--- src/main/java/study/querydsl/repository/MemberRepository.java
+++ src/main/java/study/querydsl/repository/MemberRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 // Spring Data JPA를 사용하면 인터페이스만 정의해도 구현체가 자동으로 생성된다.
 // MemberRepositoryCustom 인터페이스를 상속받아 MemberRepository 인터페이스를 만들면
-// MemberRepositoryCustom 인터페이스의 구현체도 같이 만들어진다.
+// MemberRepositoryCustom 인터페이스의 구현체인 MemberRepositoryImpl도 같이 만들어진다.
 public interface MemberRepository
         extends JpaRepository<Member, Long>, MemberRepositoryCustom {
     List<Member> findByUsername(String username);
